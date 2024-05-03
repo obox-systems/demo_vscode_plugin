850402ca6a839151d73b621e9a80df8e49517415
YuliaProkopovych <yuliaprokopovych@gmail.com>
2024-05-03 21:13:26 +0300

    command keybindings

--- src/extension.ts
+++ src/extension.ts
@@ -25,7 +25,6 @@ function assignKeybindings(keybinding: string, alias: string) {
     vscode.commands.executeCommand('extension.assignKeybinding').then(() => {
         // Add keybinding in the opened keybindings settings
         vscode.commands.executeCommand('workbench.action.openGlobalKeybindings').then(() => {
-			vscode.window.showErrorMessage(keybinding + 'assigned');
             vscode.commands.executeCommand('editor.action.addKeybinding', {
                 keybinding,
                 command: `extension.${alias}`,
@@ -66,12 +65,12 @@ function registerAliasCommands(context: vscode.ExtensionContext) {
 
 		const keybinding = customCommand.keybinding;
 
-		if(keybinding) {
-			vscode.window.showErrorMessage(keybinding);
-			// Assign keybindings dynamically
-			const disposable = assignKeybindings(keybinding, alias);
-			context.subscriptions.push(disposable);
-		}
+		// if(keybinding) {
+		// 	vscode.window.showErrorMessage(keybinding);
+		// 	// Assign keybindings dynamically
+		// 	const disposable = assignKeybindings(keybinding, alias);
+		// 	context.subscriptions.push(disposable);
+		// }
 
 		// // Define the new keybinding
 		// const newKeybinding = {
@@ -102,7 +101,6 @@ async function executeAliasCommand(alias: string, utility: string) {
 	const highlightedText = editor.document.getText(highlightedRange);
 
 	const { exec } = require('child_process');
-	const binaryPath = 'test_command';
 	exec(utility + ' ' + highlightedText, (err: any, stdout: any, stderr: any) => {
 		if (err) {
 			vscode.window.showErrorMessage(`Error: ${err}`);
@@ -120,6 +118,33 @@ async function executeAliasCommand(alias: string, utility: string) {
 			editBuilder.replace(highlightedRange, stdout + ' ' + utility);
 		});
 	});
+}
+
+// Function to select the command by its keybinding
+async function selectAliasCommand(keybinding: string) {
+
+	// Get the configuration for the current workspace
+	const config = vscode.workspace.getConfiguration();
+
+	// Get the value of customModification.custom from the configuration
+	const custom = config.get<CustomCommand[]>('customModification.custom') || [];
+
+	// Create an array of QuickPick items representing custom commands
+	let customCommands: vscode.QuickPickItem[] = [];
+
+	let utility: string;
+	// Register a command for each alias
+	custom.forEach((customCommand) => {
+		//const alias = customCommand.alias;
+		const commandUtility = customCommand.path;
+		//const label = customCommand.label || alias;
+		const commandKeybinding = customCommand.keybinding;
+		if(commandKeybinding == keybinding) {
+			utility = commandUtility;
+		}
+	});
+
+	return utility;
 }
 
 const listCommandsCommand = vscode.commands.registerCommand('extension.listCustomCommands', () => {
@@ -154,12 +179,51 @@ const listCommandsCommand = vscode.commands.registerCommand('extension.listCusto
     });
 });
 
+const executeCustomCommand = vscode.commands.registerCommand('extension.customKeybings', () => {
+
+	// Get the configuration for the current workspace
+	const config = vscode.workspace.getConfiguration();
+
+	// Get the value of customModification.custom from the configuration
+	const custom = config.get<CustomCommand[]>('customModification.custom') || [];
+
+	// Create an array of QuickPick items representing custom commands
+	let customCommands: vscode.QuickPickItem[] = [];
+	// Register a command for each alias
+	custom.forEach((customCommand) => {
+		const alias = customCommand.alias;
+		const utility = customCommand.path;
+		const keybinding = customCommand.keybinding;
+		const label = customCommand.label || alias;
+		const detail = customCommand.detail || 'Modify with ' + utility;
+		customCommands.push({ label, detail })
+	});
+
+    // Show the QuickPick menu to the user
+    vscode.window.showQuickPick(customCommands).then(selectedItem => {
+        if (selectedItem) {
+			custom.forEach((customCommand) => {
+				const label = customCommand.label || customCommand.alias;
+				if (selectedItem.label == label) {
+					executeAliasCommand(customCommand.alias, customCommand.path)
+				}
+			});
+        }
+    });
+});
+
 export function activate(context: vscode.ExtensionContext) {
 	registerAliasCommands(context);
 
 	// Subscribe to the configuration change event
-	const disposable = vscode.workspace.onDidChangeConfiguration((event: vscode.ConfigurationChangeEvent) => { handleConfigurationChange(context, event);});
+	//const disposable = vscode.workspace.onDidChangeConfiguration((event: vscode.ConfigurationChangeEvent) => { handleConfigurationChange(context, event);});
 	context.subscriptions.push(listCommandsCommand);
 	// Add the disposable to the context subscriptions so it gets disposed when the extension is deactivated
-	context.subscriptions.push(disposable);
+	//context.subscriptions.push(disposable);
+
+	    // Register the command without contributing it to the Command Palette
+		context.subscriptions.push(vscode.commands.registerCommand('extension.customPlaceholder3', () => {
+			vscode.window.showErrorMessage("placeholder 3");
+			//handleMyCommand();
+		}));
 }
